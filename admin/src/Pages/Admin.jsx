75077f4a820cd72e3fb135bacc273bd099b97695
DIYA EL FADHIL <133634488+diya-el-fadhil@users.noreply.github.com>
2024-07-21 01:29:09 +0530

    Revert "Added admin user management"
    This reverts commit d37e72adc83fa67aadfa5a3cc79324b3df694f77.

--- admin/src/Pages/Admin.jsx
+++ admin/src/Pages/Admin.jsx
@@ -2,18 +2,17 @@ import React from "react";
 import "./CSS/Admin.css";
 import Sidebar from "../Components/Sidebar/Sidebar";
 import AddProduct from "../Components/AddProduct/AddProduct";
-import ListProduct from "../Components/ListProduct/ListProduct";
-import UserManagement from "../Components/Usermng/UserManagement"; // Import the new component
 import { Route, Routes } from "react-router-dom";
+import ListProduct from "../Components/ListProduct/ListProduct";
 
 const Admin = () => {
+
   return (
     <div className="admin">
       <Sidebar />
       <Routes>
         <Route path="/addproduct" element={<AddProduct />} />
         <Route path="/listproduct" element={<ListProduct />} />
-        <Route path="/usermanagement" element={<UserManagement />} /> {/* Add the route */}
       </Routes>
     </div>
   );
