0c98b9311966ede5f7d12b6d0307eeaee723c6c4
DIYA EL FADHIL <133634488+diya-el-fadhil@users.noreply.github.com>
2024-07-22 20:10:21 +0530

    product page fixed

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -8,31 +8,28 @@ import ShopCategory from "./Pages/ShopCategory";
 import LoginSignup from "./Pages/LoginSignup";
 import Profile from "./Pages/Profile";
 
-
 export const backend_url = 'http://localhost:4000';
 export const currency = '₹';
 
 function App() {
-
   return (
     <div>
       <Router>
         <Navbar />
-        <Routes>
-          <Route path="/" element={<Shop gender="all" />} />
-          <Route path="/products" element={<ShopCategory category="product" />} />
-          <Route path='/product' element={<Product />}>
-            <Route path=':productId' element={<Product />} />
-          </Route>
-          <Route path="/cart" element={<Cart />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/login" element={<LoginSignup/>} />
-        </Routes>
+        <main>
+          <Routes>
+            <Route path="/" element={<Shop gender="all" />} />
+            <Route path="/products" element={<ShopCategory category="product" />} />
+            <Route path="/product/:productId" element={<Product />} />
+            <Route path="/cart" element={<Cart />} />
+            <Route path="/profile" element={<Profile />} />
+            <Route path="/login" element={<LoginSignup />} />
+          </Routes>
+        </main>
         <Footer />
       </Router>
     </div>
   );
 }
-
 
 export default App;
