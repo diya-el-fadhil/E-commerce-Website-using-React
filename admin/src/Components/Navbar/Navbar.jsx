90a49a2990f17bd9c080e932e8efc47468160297
DIYA EL FADHIL <133634488+diya-el-fadhil@users.noreply.github.com>
2024-07-22 20:03:05 +0530

    removed Sidebar

--- admin/src/Components/Navbar/Navbar.jsx
+++ admin/src/Components/Navbar/Navbar.jsx
@@ -1,12 +1,29 @@
 import React from 'react'
 import './Navbar.css'
-import navprofileIcon from '../Assets/nav-profile.svg'
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className='navbar'>
       <h2>STORED Admin Pannel</h2>
+      <Link to='/addproduct' style={{ textDecoration: 'none' }}>
+        <div className="sidebar-item">
+          <p>Add Product</p>
+        </div>
+      </Link>
+      <Link to='/listproduct' style={{ textDecoration: 'none' }}>
+        <div className="sidebar-item">
+          <p>Product List</p>
+        </div>
+      </Link>
+      <Link to='/usermanagement' style={{ textDecoration: 'none' }}>
+        <div className="sidebar-item">
+          <p>User Management</p>
+        </div>
+      </Link>
     </div>
+    
+    
   )
 }
 
